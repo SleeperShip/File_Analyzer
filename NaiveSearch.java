efbf69dae47ecbe5713264bee4c49b6303b59b2d
star_engine <jds4873@gmail.com>
2022-04-15 15:19:39 -0600

    Update NaiveSearch.java

--- NaiveSearch.java
+++ NaiveSearch.java
@@ -1,21 +1,21 @@
 public class NaiveSearch {
  
     public static boolean naiveStringSearch(String txt, String pat) {
-        int M = pat.length();
-        int N = txt.length();
+        int pattLen = pat.length(); 
+        int txtLen = txt.length();  
  
         /* A loop to slide pat one by one */
-        for (int i = 0; i <= N - M; i++) {
+        for (int i = 0; i <= textLen - pattLen; i++) {
  
             int j;
  
             /* For current index i, check for pattern
               match */
-            for (j = 0; j < M; j++)
-                if (txt.charAt(i + j) != pat.charAt(j))
+            for (j = 0; j < pattLen; j++)
+                if (txt.charAt(i + j) != pattern.charAt(j))
                     break;
  
-            if (j == M) // if pat[0...M-1] = txt[i, i+1, ...i+M-1]
+            if (j == pattLen) // if pat[0...M-1] = txt[i, i+1, ...i+M-1]
                 //System.out.println("Pattern found at index " + i);
                 return true;
         }
